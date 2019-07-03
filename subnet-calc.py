1718dd0e6071cc7891257449dff450546c91d6d9
sam-augustyn <sam.augustyn@gmail.com>
2019-07-03 15:01:20 -0400

    added comments to subnet calc

--- subnet-calc.py
+++ subnet-calc.py
@@ -6,9 +6,13 @@ interface = "wlp2s0"
 ipaddressRegex = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
 
 process = subprocess.Popen(['ifconfig', interface], stdout=subprocess.PIPE)
-stdout = (process.communicate()[0]).decode("utf-8")
-results = re.findall(ipaddressRegex, stdout)
+#return an array of the all the matches in the string after its been decoded
+results = re.findall(ipaddressRegex, (process.communicate()[0]).decode("utf-8"))
+#get the number of 1s in the result
 netmask = IPAddress(results[1]).netmask_bits()
+#determine the max amount of hostss
 slash = 2 ** (32 - netmask) - 1
+#get the broadcast address
 broadcast = IPAddress(results[2])
+#return the slash notation of the network
 print(str(broadcast - slash) + '/' + str(netmask))
